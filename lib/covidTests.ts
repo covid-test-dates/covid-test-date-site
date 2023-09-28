a9d9771afd04a2ed183aacc2e0564fcbf2eedb1f
Alan Gerber <akgerber@users.noreply.github.com>
2023-09-28 10:56:15 -0400

    Consistent casing

--- lib/covidTests.ts
+++ lib/covidTests.ts
@@ -2,7 +2,7 @@ import { parse } from 'csv-parse/sync';
 import fs from 'fs';
 import path from 'path';
 
-const c19_tests_file = path.join(process.cwd(), 'covid-tests', 'c19_test_spreadsheet_export.csv');
+const c19TestsFile = path.join(process.cwd(), 'covid-tests', 'c19_test_spreadsheet_export.csv');
 
 export type TestManufacturer = string
 export type TestBrandName = string
@@ -15,7 +15,7 @@ export type C19TestData = [TestManufacturer, TestBrandName, LotNumber, OriginalE
 export function getSortedTestsData(): C19TestData[] {
     // Load all COVID test data from CSV files in /tests
     const result: C19TestData[] = [];
-    const c19_test_data_raw = fs.readFileSync(c19_tests_file);
-    const c19_test_data = parse(c19_test_data_raw, { delimiter: ",", from_line: 2 });
-    return c19_test_data;
+    const c19TestDataRaw = fs.readFileSync(c19TestsFile);
+    const c19TestData = parse(c19TestDataRaw, { delimiter: ",", from_line: 2 });
+    return c19TestData;
 }
