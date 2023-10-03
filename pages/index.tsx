956beaa9989dd49eb7e936a8506f430e5160728b
Alan Gerber <akgerber@users.noreply.github.com>
2023-10-02 23:44:06 -0400

    Get reactive logic filtering tests by manufacturer mostly working

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import { GetStaticProps } from 'next';
-import { C19TestData, getSortedTestsData, TestManufacturer } from '@/lib/covidTests';
+import { C19TestData as C19TestData, getSortedTestsData, TestBrandName, TestManufacturer } from '@/lib/covidTests';
 import { Container, Select } from '@mantine/core';
 import { Dispatch, SetStateAction, useState } from 'react';
 import { DatePicker } from '@mantine/dates';
@@ -12,15 +12,18 @@ import { ColorSchemeScript } from '@mantine/core';
 export default function Home({
   c19TestData,
   manufacturers,
+  testBrands,
 }:
   {
     c19TestData: C19TestData[];
     manufacturers: TestManufacturer[];
+    testBrands: TestBrandName[];
   }
 ) {
-  const [expiration, setExpiration] = useState<Date | null>(null);
   const [manufacturer, setManufacturer] = useState<TestManufacturer | null>(null);
-  console.log(c19TestData);
+  const [testBrand, setTestBrand] = useState<TestBrandName | null>(null);
+  const [expiration, setExpiration] = useState<Date | null>(null);
+
   return (
     <>
       <Head>
@@ -35,11 +38,20 @@ export default function Home({
           </p>
         </section>
         <section>
-          <TestManufacturerSelector
-            manufacturers={manufacturers}
+          <C19TestSelector
+            label="COVID Test Manufacturer"
+            options={manufacturers}
             value={manufacturer}
+            disabled={false}
             setValue={setManufacturer} />
           {manufacturer ? <p>The selected manufacturer is {manufacturer}</p> : null}
+          <C19TestSelector
+            label="COVID Test Brand Name"
+            options={Array.from(new Set(c19TestData.filter((row) => row[0] === manufacturer).map((row) => row[1])))}
+            value={testBrand}
+            disabled={manufacturer === null}
+            setValue={setTestBrand} />
+          {manufacturer ? <p>The selected test brand is {testBrand}</p> : null}
           <h2>Original Expiration Date</h2>
           <OriginalExpirationDate value={expiration} setValue={setExpiration} />
           {expiration ? <p>The selected date is {expiration.toLocaleString()}</p> : null}
@@ -49,25 +61,27 @@ export default function Home({
   );
 }
 
-interface TestManufacturers {
-  manufacturers: TestManufacturer[],
-  value: TestManufacturer | null,
-  setValue: Dispatch<SetStateAction<TestManufacturer | null>>
+interface C19TestProps {
+  label: string,
+  disabled: boolean,
+  options: string[],
+  value: string | null,
+  setValue: Dispatch<SetStateAction<string | null>>
 }
 
-function TestManufacturerSelector({ manufacturers, value, setValue }: TestManufacturers) {
+function C19TestSelector<T>({ label, disabled, options, value, setValue }: C19TestProps) {
   return (
     <Select
       label="COVID Test Manufacturer"
       placeholder="Pick value"
-      data={manufacturers}
+      data={options}
       value={value}
       onChange={setValue}
+      disabled={disabled}
       searchable
     />
   );
 }
-
 
 function OriginalExpirationDate({ value, setValue }: { value: Date | null, setValue: Dispatch<SetStateAction<Date | null>> }) {
   return <DatePicker value={value} onChange={setValue} />;
@@ -78,10 +92,13 @@ function OriginalExpirationDate({ value, setValue }: { value: Date | null, setVa
 export const getStaticProps: GetStaticProps = async () => {
   const c19TestData = getSortedTestsData();
   const manufacturers: TestManufacturer[] = Array.from(new Set(c19TestData.map((row) => row[0])))
+  const testBrands: TestBrandName[] = Array.from(new Set(c19TestData.map((row) => row[1])))
+  const testBrandsByManufacturer: TestBrandName[] = Array.from(new Set(c19TestData.map((row) => row[1])))
   return {
     props: {
       c19TestData,
       manufacturers,
+      testBrands
     },
   };
 };
