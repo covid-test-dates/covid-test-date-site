58a9a1a7849e561c228fdde09466462b04ba6337
Samir Lavingia <lavingiasa@gmail.com>
2023-10-03 21:42:16 -0400

    Update the color scheme
    Update the color scheme to https://v7.mantine.dev/colors-generator?color=635378

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,25 @@ import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
 
 import type { AppProps } from 'next/app';
-import { MantineProvider, createTheme } from '@mantine/core';
+import { MantineProvider, createTheme, MantineColorsTuple } from '@mantine/core';
+
+const myColor: MantineColorsTuple = [
+  '#f7f2fb',
+  '#e7e3ec',
+  '#cbc4d4',
+  '#afa5bd',
+  '#9789a9',
+  '#88779d',
+  '#806f97',
+  '#6f5d84',
+  '#625277',
+  '#56466b'
+];
 
 const theme = createTheme({
-    /** Put your mantine theme override here */
+  colors: {
+    myColor,
+  }
 });
 
 export default function App({ Component, pageProps }: AppProps) {
