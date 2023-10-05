a4c6ee4c83e128e311b4761796b9e2a29f86f6ad
Samir Lavingia <lavingiasa@gmail.com>
2023-10-05 15:47:45 -0400

    Updating favicon
    Following https://nextjs.org/docs/app/api-reference/file-conventions/metadata/app-icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home({
       <Head>
         <ColorSchemeScript defaultColorScheme="auto" />
         <title>TurboTest 🚀🧪</title>
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧪</text></svg>"/>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
         <meta
           name="description"
           content="Check your updated COVID test expiration dates here!"
