9a92325618e0b5a801fb3bc5163c975a60e0d75a
Alan Gerber <akgerber@users.noreply.github.com>
2023-10-05 17:45:20 -0400

    Downgrade jest-dom to 5.x to fix tests

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -4,7 +4,6 @@ import { screen } from '@testing-library/react';
 import { describe, expect, it } from '@jest/globals';
 import Home from '../pages/index';
 import '@testing-library/jest-dom';
-import { Container, Select } from '@mantine/core';
 import { render } from '@/test-utils';
 
 
