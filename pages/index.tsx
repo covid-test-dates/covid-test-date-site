81f57765b8ce8075c6434c3e99bb95fccaafe517
Samir Lavingia <lavingiasa@gmail.com>
2023-10-05 15:31:17 -0400

    Fixing png to jpg

--- pages/index.tsx
+++ pages/index.tsx
@@ -61,7 +61,7 @@ export default function Home({
         />
         <meta
           property="og:image"
-          content="https://storage.googleapis.com/cookieboi.com/covid-test/turbotest.png"
+          content="https://storage.googleapis.com/cookieboi.com/covid-test/turbotest.jpg"
         />
 
         {/* <!-- Twitter Meta Tags --> */}
@@ -75,7 +75,7 @@ export default function Home({
         />
         <meta
           name="twitter:image"
-          content="https://storage.googleapis.com/cookieboi.com/covid-test/turbotest.png"
+          content="https://storage.googleapis.com/cookieboi.com/covid-test/turbotest.jpg"
         />
 
         {/* <!-- Meta Tags Generated via https://www.opengraph.xyz --> */}
